4032083456fbb4f482f8b2106d203062069ce5f8
Tianchi YU <yu-tianchi@outlook.com>
2023-09-24 19:01:49 +0200

    update logo size

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,9 +15,9 @@
 
     // === logo change
     if (ud_header.classList.contains("sticky")) {
-      logo.src = "assets/images/logo/ts-logo-black-small.svg";
-    } else {
       logo.src = "assets/images/logo/ts-logo-blue-small.svg";
+    } else {
+      logo.src = "assets/images/logo/ts-logo-black-small.svg";
     }
 
     // show or hide the back-top-top button
